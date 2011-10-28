6d98ae51df7cac4a889cf5dffc2e3d3a8c4079b8
justinpfister <justin@openyard.com>
2011-10-28 00:10:52 -0400

    building the initial file base

--- file-mods.sh
+++ file-mods.sh
@@ -7,3 +7,7 @@ sudo cp resources/html/index.php /usr/local/nginx/html/index.php
 sudo cp resources/html/watts.jpg /usr/local/nginx/html/watts.jpg
 
 sudo cp perl/resize.pm /etc/perl/resize.pm
+
+sudo chown -R www-data:webdev /usr/local/nginx/html
+
+sudo chmod -R ug=wrx,o= /usr/local/nginx/html
