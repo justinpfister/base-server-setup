d4061ce3d0bd66b3d8f6defabb9260e5f5565644
justinpfister <justin@openyard.com>
2011-10-28 00:01:42 -0400

    building the initial file base

---
+++ resources/html/index.php
@@ -0,0 +1,15 @@
+<ul>
+<li><img src="http://justinpfister.com/watts.resize_to.100x100.jpg"></li>
+<li><img src="http://justinpfister.com/watts.resize_to.200x200.jpg"></li>
+<li><img src="http://justinpfister.com/watts.resize_to.300x300.jpg"></li>
+<li><img src="http://justinpfister.com/watts.resize_to.400x400.jpg"></li>
+</ul>
+
+<?
+
+echo "test";
+
+echo phpinfo();
+
+?>
+
