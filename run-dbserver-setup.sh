d98d45a15ee0fef09d95b70fd926e46404cefd4a
unknown <user@MADISONATOR.(none)>
2011-10-28 16:27:10 -0400

    testing update script

--- run-dbserver-setup.sh
+++ run-dbserver-setup.sh
@@ -7,5 +7,7 @@ sudo apt-get upgrade
 sudo apt-get -y install nginx php5-fpm
 
 sudo debconf-set-selections <<< 'tempsqlpw'
+
 sudo debconf-set-selections <<< 'tempsqlpw'
+
 sudo apt-get -y install mysql-server
