205c893ac1f70a9a81d738e1f7a6646afa8da924
justinpfister <justin@openyard.com>
2011-10-28 00:57:25 -0400

    working around the mysql password prompt

--- webapp-setup.sh
+++ webapp-setup.sh
@@ -15,4 +15,6 @@ sudo apt-get -y install memcached php5-memcache php-apc
 ## Install MySQL
 ###########################
 
+sudo debconf-set-selections <<< 'tempsqlpw'
+sudo debconf-set-selections <<< 'tempsqlpw'
 sudo apt-get -y install mysql-server
