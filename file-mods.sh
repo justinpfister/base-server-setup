44c3c8426f5e9ea3a868068a96312540d572b715
justinpfister <justin@openyard.com>
2011-10-28 00:35:16 -0400

    modified readme with shell header.. for easier deployment

--- file-mods.sh
+++ file-mods.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo /bin/cp -Rf configs/nginx.conf /usr/local/nginx/conf/nginx.conf
-
 sudo cp resources/html/index.php /usr/local/nginx/html/index.php
 
 sudo cp resources/html/watts.jpg /usr/local/nginx/html/watts.jpg
--- init-scripts/setup-nginx.sh
+++ init-scripts/setup-nginx.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+sudo /bin/cp -Rf configs/nginx.conf /usr/local/nginx/conf/nginx.conf
+
+sudo /bin/cp -Rf configs/fastcgi_params /usr/local/nginx/conf/fastcgi_params
+
 sudo touch /etc/init.d/nginx
 
 sudo /bin/cp -Rf init-scripts/nginx.sh /etc/init.d/nginx
@@ -9,3 +13,5 @@ sudo chmod +x /etc/init.d/nginx
 sudo /usr/sbin/update-rc.d -f nginx defaults
 
 sudo /etc/init.d/nginx restart
+
+sudo /etc/init.d/php5-fpm restart
