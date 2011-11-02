20eb94a76a75ee3b5e4cd0a7c47694b9a0a6423f
justinpfister <justin@openyard.com>
2011-11-02 13:35:48 -0400

    added php5-gd

--- webapp-setup.sh
+++ webapp-setup.sh
@@ -9,6 +9,6 @@ sudo apt-get -y install php5-cli php5-common php5-suhosin
 
 sudo apt-get -y install php5-fpm php5-cgi
 
-sudo apt-get -y install memcached php5-memcache php-apc php5-mysql php5-curl
+sudo apt-get -y install memcached php5-memcache php-apc php5-mysql php5-curl php5-gd
 
 
