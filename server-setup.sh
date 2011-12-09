f620ad987f6ba0a6c43ddf89f001f36e09c0dfec
justinpfister <justin@openyard.com>
2011-12-09 14:02:31 -0500

    updated some sprod.kyotist details.

--- server-setup.sh
+++ server-setup.sh
@@ -17,7 +17,7 @@ sudo aptitude -y install build-essential
 
 sudo apt-get -y install python-software-properties
 
-sudo apt-get -y install libxslt1-dev libgd2-xpm-dev libxt-dev libperl-dev libgeoip-dev imagemagick perlmagick git apache2-utils acl
+sudo apt-get -y install libxslt1-dev libgd2-xpm-dev libxt-dev libperl-dev libgeoip-dev imagemagick perlmagick git apache2-utils acl yui-compressor
 
 sudo aptitude -y install libc6 libpcre3 libpcre3-dev libpcrecpp0 libssl0.9.8 libssl-dev zlib1g zlib1g-dev lsb-base ssl-cert
 
