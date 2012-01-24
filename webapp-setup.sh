e21b8e51235e79f34e5c0f82a2475e931b2e8f9d
justinpfister <justin@openyard.com>
2012-01-24 13:32:43 -0500

    app_bench -> app_benchmark

--- webapp-setup.sh
+++ webapp-setup.sh
@@ -9,6 +9,6 @@ sudo apt-get -y install php5-cli php5-common php5-suhosin
 
 sudo apt-get -y install php5-fpm php5-cgi
 
-sudo apt-get -y install memcached php5-memcache php-apc php5-mysql php5-curl php5-gd php5-sqlite php5-intl
+sudo apt-get -y install memcached php5-memcache php5-memcached php-apc php5-mysql php5-curl php5-gd php5-sqlite php5-intl
 
 
