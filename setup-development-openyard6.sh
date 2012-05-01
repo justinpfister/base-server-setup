c7041bd8f781e05cb58ed791650ef6964200ce24
justinpfister <justin@openyard.com>
2012-05-01 14:40:32 -0400

    got things pretty automated.

--- setup-development-openyard6.sh
+++ setup-development-openyard6.sh
@@ -5,8 +5,16 @@ git clone git@github.com:justinpfister/openyard6-symfony.git /var/www/developmen
 cd /var/www/development-openyard6
 
 sudo cp configs/devparameters.ini /var/www/development-openyard6/app/config/
+sudo mv /var/www/development-openyard6/app/config/devparameters.ini /var/www/development-openyard6/app/config/parameters.ini
 
 php bin/vendors install
 
+sudo setfacl -R -m u:www-data:rwx -m u:justin:rwx app/cache app/logs app/Resources/java prodimgs
+
+sudo setfacl -dR -m u:www-data:rwx -m u:justin:rwx app/cache app/logs app/Resources/java prodimgs
+
 cd ~/base-server-setup
+
+sudo chown -R www-data:webdev /var/www
+sudo chmod -R ug=wrx,o= /var/www
 
