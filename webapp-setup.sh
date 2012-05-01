50b4dcec0a3979728e10cebd0469a238a4b36032
justinpfister <justin@openyard.com>
2012-05-01 12:08:55 -0400

    modified some how-to.. ssh related.

--- webapp-setup.sh
+++ webapp-setup.sh
@@ -12,3 +12,8 @@ sudo apt-get -y install php5-fpm php5-cgi
 sudo apt-get -y install memcached php5-memcache php5-memcached php-apc php5-mysql php5-curl php5-gd php5-sqlite php5-intl
 
 
+#################################
+## Install Node.js
+##################################
+
+sudo apt-get install -y g++ curl libssl-dev apache2-utils git-core && git clone git://github.com/joyent/node.git && cd node && ./configure && make && sudo make install
