6f8dc5d6ea6f572a85878344df5118f4298399e9
justinpfister <justin@openyard.com>
2012-05-01 15:44:18 -0400

    modified.

--- run-webserver-update.sh
+++ run-webserver-update.sh
@@ -7,7 +7,7 @@ git pull
 #these are the most basic Nginx configuration files. It lays the foundation for adding other configuration files from other locations.
 
 # Update the base nginx config files
-sudo sh file-mods/nginx-configs.sh
+# sudo sh file-mods/nginx-configs.sh
 
 sudo /etc/init.d/nginx restart
 
